fd3ed37531c760d57dfab029185aabfbbe25309a
jackiepareja <pareja.jackie@gmail.com>
2022-06-20 22:31:14 -0700

    Add green border on mouse move, and two-image drag

--- scripts-1.js
+++ scripts-1.js
@@ -1,95 +1,102 @@
-function init() {
-  const CANVAS = document.getElementById('canvas');
-  const CONTEXT = canvas.getContext('2d');
-  let pieces = [];
-  let SELECTED_PIECE = null;
+(function init() {
+  const CANVAS = document.getElementById("canvas");
+  const CONTEXT = CANVAS.getContext("2d");
+  const DOG_IMG = new Image();
+  const CAT_IMG = new Image();
+  DOG_IMG.src = "./assets/dog.png";
+  CAT_IMG.src = "./assets/cat.png";
 
-  let isDragging = false;
+  // starter positions for dog image which is centered
+  let currX = CANVAS.width / 2;
+  let currY = CANVAS.height / 2;
+  let draggable = false;
 
-  const imageDog = new Image();
+  let draggableItems = [
+    {
+      itemName: "DOG_IMG",
+      xCoord: currX - DOG_IMG.width / 2,
+      yCoord: currY - DOG_IMG.height / 2,
+      draggable: false
+    },
+    {
+      itemName: "CAT_IMG",
+      xCoord: 0,
+      yCoord: 0,
+      draggable: false
+    }
+  ];
 
-  const createShape = (leftX, topY, width, height, backgroundColor) => {
-    CONTEXT.fillStyle = backgroundColor;
-    CONTEXT.fillRect(leftX, topY, width, height);
-  }
-  //  create canvas
-  createShape(0, 0, CANVAS.width, CANVAS.height, '#FAF7F8');
-
-  // renders dog image
-  imageDog.onload = function() {
-    CONTEXT.drawImage(imageDog, 20, 20, 150, 150);
-  }
-  imageDog.src = './assets/dog.png';
-  imageDog.class = "imageDog";
-
-  // Drag start
-  function handleMouseDown(e) {
-    // let xCoord = e.clientX - CANVAS.offsetLeft;
-    // let yCoord = e.clientY - CANVAS.offsetTop;
-    // isDragging = true;
-
-    SELECTED_PIECE = getPressedPiece(e);
-  }
-
-  // Drag end
-  function handleMouseUp(e) {
-    let xCoord = e.clientX - CANVAS.offsetLeft;
-    let yCoord = e.clientY - CANVAS.offsetTop;
-    isDragging = false;
-    console.log('clientX', e.clientX);
-    console.log('offset', CANVAS.offsetLeft)
+  const main = () => {
+    updateCanvas();
   }
 
-  // Clear canvas and draw the image at the mouse position
-  function handleMouseMove(e) {
-    let xCoord = e.clientX - CANVAS.offsetLeft;
-    let yCoord = e.clientY - CANVAS.offsetTop;
-    if (isDragging) {
-      CONTEXT.clearRect(0, 0, CANVAS.width, CANVAS.height);
-      CONTEXT.drawImage(imageDog, xCoord, yCoord, 150, 150)
+  const clearCanvas = () => {
+    CONTEXT.clearRect(0, 0, CANVAS.width, CANVAS.height)
+  }
+
+  const handleDragEvents = () => {
+    CANVAS.addEventListener("mousedown", onMouseDownHandler);
+    CANVAS.addEventListener("mousemove", onMouseMoveHandler);
+    CANVAS.addEventListener("mouseup", onMouseUpHandler);
+  }
+  
+  const onMouseDownHandler = (evt) => {
+    let isXBounds = evt.layerX <= (currX + DOG_IMG.width / 2) && 
+      evt.layerX >= (currX - DOG_IMG.width / 2);
+    let isYBounds = evt.layerY <= (currY + DOG_IMG.width / 2) && 
+      evt.layerY >= (currY - DOG_IMG.width / 2);
+    // detect if the click was on the image
+    if(isXBounds && isYBounds) {
+      draggable = true;
+    } 
+    else {
+      console.log('didnt click image')
     }
   }
 
-  CANVAS.addEventListener('mousedown', handleMouseDown)
-  CANVAS.addEventListener('mouseup', handleMouseUp)
-  CANVAS.addEventListener('mousedown', handleMouseMove)
+  const onMouseMoveHandler = (evt) => {
+    if(draggable) {
+      currX = evt.layerX;
+      currY = evt.layerY;
+    }
+  }
 
-}
+  const onMouseUpHandler = (evt) => {
+    draggable = false;
+  }
 
-window.addEventListener('load', init);
+  const updateCanvas = () => {
 
+    window.onload = () => {
+      setInterval(() => {
+        clearCanvas();
+        CONTEXT.drawImage(
+          DOG_IMG, currX - DOG_IMG.width / 2,
+          currY - DOG_IMG.height / 2)
+        CONTEXT.drawImage(
+          CAT_IMG, 0, 0
+        )
+      }, 200)
+    }
 
+    // DOG_IMG.onload = () => {
+    //   setInterval(() => {
+    //     clearCanvas();
+    //     CONTEXT.drawImage(
+    //       DOG_IMG, currX - DOG_IMG.width / 2, 
+    //       currY - DOG_IMG.height /2)
+    //   }, 200)}
+    
+      
+    //   CAT_IMG.onload = () => {
+    //     setInterval(() => {
+    //       CONTEXT.drawImage(
+    //         CAT_IMG, 0, 0
+    //       )
+    //     }, 200)
+    //   }
+  }
 
-
-
-
-
-
-
-
-  //const imageCat = new Image();
-  // renders cat image
-  // imageCat.onload = function() {
-  //   CONTEXT.drawImage(imageCat, 100, 300, 150, 150)
-  // }
-  // imageCat.src = './assets/cat.png';
-
-  // const colorCircle = (centerX, centerY, radius, drawColor) => {
-  //   CONTEXT.fillStyle = drawColor;
-  //   CONTEXT.beginPath();
-
-  //   CONTEXT.arc(centerX, centerY, radius, 0, Math.PI * 2, true);
-
-  //   CONTEXT.fill();
-  // }
-
-  // const drawNet = () => {
-  //   for (let i = 0; i < canvas.height; i+= 40) {
-  //     createShape(canvas.width / 2 -1, i, 2, 20, "red");
-  //   }
-  // }
-
-  // drawNet();
-
-  // colorCircle(300, 600, 10, "red");
-  // CONTEXT.fillText("GO", 300, 600, "white" )
+  main();
+  handleDragEvents();
+}())
--- scripts.js
+++ scripts.js
@@ -1,88 +1,143 @@
-(function init() {
+/**
+ * TO DO:
+ * 1. Add conditional that if both images are touching each other, the top (z-level) is the only clickable one, unless the bottom level one is visible and clickable.
+ * 2. Center the canvas without affecting the x, y orientations
+ * 3. When clicked and dragged, remove the clear canvas on the image bug
+ * 4. Remove the green border re-drawing
+ */
 
+(function init() {
   const CANVAS = document.getElementById("canvas");
   const CONTEXT = CANVAS.getContext("2d");
   const DOG_IMG = new Image();
   const CAT_IMG = new Image();
+  DOG_IMG.src = "./assets/dog.png";
+  CAT_IMG.src = "./assets/cat.png";
 
-  // starter positions for dog image which is centered
-  let currX = CANVAS.width / 2;
-  let currY = CANVAS.height / 2;
-  let draggable = false;
+  /**
+   * Array of all draggable items on the canvas.  
+   */ 
+  let draggableItems = [
+    {
+      itemName: "DOG_IMG",
+      currX: CANVAS.width / 2,
+      currY: CANVAS.height / 2,
+      width: DOG_IMG.width,
+      height: DOG_IMG.height,
+      draggable: false
+    },
+    {
+      itemName: "CAT_IMG",
+      currX: 100,
+      currY: 100,
+      width: CAT_IMG.width,
+      height: CAT_IMG.height,
+      draggable: false
+    }
+  ];
 
-  const main = () => {
-    updateCanvas();
+  /**
+   * Draws and re-draws the items on the canvas.
+   */
+  window.onload = () => {
+    setInterval(() => {
+      clearCanvas();
+      draw();
+    }, 200)
   }
 
+  /**
+   * Draws on the canvas.
+   */
+  const draw = () => {
+    for (let item of draggableItems) {
+      if(item.itemName === "DOG_IMG") {
+        CONTEXT.drawImage(
+          DOG_IMG, item.currX - item.width / 2, item.currY - item.height / 2
+        )
+      }
+      if(item.itemName === "CAT_IMG") {
+        CONTEXT.drawImage(
+          CAT_IMG, item.currX - item.width / 2, item.currY - item.height / 2
+        )
+      }
+    }
+  }
+
+  /**
+   * Clears the canvas.
+   */
   const clearCanvas = () => {
     CONTEXT.clearRect(0, 0, CANVAS.width, CANVAS.height)
   }
 
+  /**
+   * Drag event handlers.
+   */
   const handleDragEvents = () => {
     CANVAS.addEventListener("mousedown", onMouseDownHandler);
     CANVAS.addEventListener("mousemove", onMouseMoveHandler);
     CANVAS.addEventListener("mouseup", onMouseUpHandler);
   }
   
+  /**
+   * @event evt - mouse down event
+   */
   const onMouseDownHandler = (evt) => {
-    let isXBounds = evt.layerX <= (currX + DOG_IMG.width / 2) && 
-      evt.layerX >= (currX - DOG_IMG.width / 2);
-    let isYBounds = evt.layerY <= (currY + DOG_IMG.width / 2) && 
-      evt.layerY >= (currY - DOG_IMG.width / 2);
-    // detect if the click was on the image
-    if(isXBounds && isYBounds) {
-      draggable = true;
-    } 
-    else {
-      console.log('didnt click image')
+    for(let item of draggableItems) {
+      let isXBounds = evt.layerX <= (item.currX + item.width / 2) && evt.layerX >= (item.currX - item.width / 2);
+      let isYBounds = evt.layerY <= (item.currY + item.width / 2) && evt.layerY >= (item.currY - item.width / 2);
+      if(isXBounds && isYBounds) {
+        item.draggable = true;
+        addGreenBorder(item);
+      }
     }
   }
 
+  /**
+   * @event evt - mouse move event
+   */
   const onMouseMoveHandler = (evt) => {
-    if(draggable) {
-      currX = evt.layerX;
-      currY = evt.layerY;
+    for(let item of draggableItems) {
+      if(item.draggable) {
+        item.currX = evt.layerX;
+        item.currY = evt.layerY;
+        addGreenBorder(item);
+        clearGreenBorder(item);
+      }
     }
   }
 
+  /**
+   * @event evt - mouse up event
+   */
   const onMouseUpHandler = (evt) => {
-    draggable = false;
-  }
-
-  const updateCanvas = () => {
-    DOG_IMG.src = "./assets/dog.png";
-    CAT_IMG.src = "./assets/cat.png";
-
-    window.onload = () => {
-      setInterval(() => {
-        clearCanvas();
-        CONTEXT.drawImage(
-          DOG_IMG, currX - DOG_IMG.width / 2,
-          currY - DOG_IMG.height / 2)
-        CONTEXT.drawImage(
-          CAT_IMG, 0, 0
-        )
-      }, 200)
+    for(let item of draggableItems) {
+      item.draggable = false;
     }
-
-    // DOG_IMG.onload = () => {
-    //   setInterval(() => {
-    //     clearCanvas();
-    //     CONTEXT.drawImage(
-    //       DOG_IMG, currX - DOG_IMG.width / 2, 
-    //       currY - DOG_IMG.height /2)
-    //   }, 200)}
-    
-      
-    //   CAT_IMG.onload = () => {
-    //     setInterval(() => {
-    //       CONTEXT.drawImage(
-    //         CAT_IMG, 0, 0
-    //       )
-    //     }, 200)
-    //   }
   }
 
-  main();
+  /**
+   * 
+   * @param {object} item - adds green border around clicked item
+   */
+  const addGreenBorder = (item) => {
+    CONTEXT.beginPath();
+    CONTEXT.strokeRect(item.currX - item.width / 2, item.currY - item.height / 2, item.width, item.height);
+    CONTEXT.lineWidth = 2;
+    CONTEXT.strokeStyle = "green";
+    CONTEXT.closePath();
+    clearGreenBorder(item);
+  }
+
+  /**
+   * 
+   * @param {*} item - removes green border
+   */
+  const clearGreenBorder = (item) => {
+    CONTEXT.clearRect(item.currX - item.width / 2, item.currY - item.height / 2, item.width, item.height);
+  }
+
+  // Invoke mouse events immediately.
   handleDragEvents();
 }())
